01021cc7bee1fd4dc9343c02c1e5b30ffaae50cd
Hurshal Patel <hurshal.patel@adroll.com>
2013-07-17 11:50:49 -0700

    use parentheses with print in Python3

--- compare.py
+++ compare.py
@@ -3,12 +3,12 @@
 
 def stuff(guess,answer):
     if guess > answer:
-        print 'Too high, go for a lower guess'
+        print('Too high, go for a lower guess')
         return False
     if guess < answer:
-        print 'Too low, go for a higher guess'
+        print('Too low, go for a higher guess')
         return False
     if guess == answer:
-        print 'Correct, lets play again'
+        print('Correct, lets play again')
         return True
         
--- run_this_file.py
+++ run_this_file.py
@@ -1,12 +1,12 @@
 import dice
 import compare
 
-print 'Instructions:'
-print 'You have 10 tries to guess what the number I am thinking of is'
-print 'My number is between 1 and 100'
-print 'Type q to quit'
-print
-print
+print('Instructions:')
+print('You have 10 tries to guess what the number I am thinking of is')
+print('My number is between 1 and 100')
+print('Type q to quit')
+print()
+print()
 
 
 guess = None
@@ -16,8 +16,9 @@ while True:
     while guess != answer:
         guess = input('What number am I thinking of: ')
         if guess == 'q':
-            print 'Bye!'
+            print('Bye!')
             exit()
         correct = compare.stuff(guess,answer)
+
         if correct:
             break
