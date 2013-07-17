31f10ff24a6e94fff3da0a160c9bdc5babe9fbbf
Hurshal Patel <hurshal.patel@adroll.com>
2013-07-17 11:33:06 -0700

    add boolean return values to compare

--- compare.py
+++ compare.py
@@ -1,8 +1,11 @@
 def stuff(guess,answer):
     if guess > answer:
         print 'Too high, go for a lower guess'
+        return False
     if guess < answer:
         print 'Too low, go for a higher guess'
+        return False
     if guess == answer:
         print 'Correct, lets play again'
+        return True
         
