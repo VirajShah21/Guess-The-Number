e0b61d35a57a17960cb62482eb7d40066457258f
Hurshal Patel <hurshal.patel@adroll.com>
2013-07-17 11:38:52 -0700

    add exiting

--- run_this_file.py
+++ run_this_file.py
@@ -3,6 +3,7 @@ import dice ; import compare
 print 'Instructions:'
 print 'You have 10 tries to guess what the number I am thinking of is'
 print 'My number is between 1 and 100'
+print 'Type q to quit'
 print
 print
 
@@ -13,4 +14,7 @@ while True:
     answer = dice.dice()
     while guess != answer:
         guess = input('What number am I thinking of: ')
+        if guess == 'q':
+            print 'Bye!'
+            exit()
         compare.stuff(guess,answer)
