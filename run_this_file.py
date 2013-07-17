69bcf36e61154ffd4448ba2e5c41d46ffe3a246a
Hurshal Patel <hurshal.patel@adroll.com>
2013-07-17 11:57:41 -0700

    add comments to run

--- run_this_file.py
+++ run_this_file.py
@@ -12,19 +12,27 @@ print()
 guess = None
 
 while True:
+    # initialize answer and try counter
     answer = dice.dice()
     tries = 0
     while guess != answer:
         guess = input('What number am I thinking of: ')
+
+        # exit if player hits q
         if guess == 'q':
             print('Bye!')
             exit()
+
+        # player tried again
         tries += 1
+        # check against answer
         correct = compare.stuff(int(guess),answer)
 
+        # player won, start a new game
         if correct:
             break
 
+        # player ran out of tries, start a new game
         if tries == 10:
             print('Sorry, your 10 tries are up. The number was %s. Lets play again.' % answer)
             break
