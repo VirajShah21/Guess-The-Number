14203287b2bc1cf35c39b735c327ce3ff0099168
Hurshal Patel <hurshal.patel@adroll.com>
2013-07-18 12:50:39 -0700

    restructure logic

--- run_this_file.py
+++ run_this_file.py
@@ -18,35 +18,30 @@ while True:
         guess = raw_input('What number am I thinking of: ')
         #checks value if can be converted into an integer 
         #and converts it if possible
+        #then uses answer as a guess
         if guess.isdigit():
             guess = int(guess)
-        #checks value if it contains ANY letters in it
-        if type(guess) == str:
-            if guess.isalpha():
-                guess = str(guess)
-                if guess == 'q':
-                    lol = 'lmfao'
-                else:
-                    print('This command is invalid')
-            
-        
-
-        # exit if player hits q
-        if guess == 'q':
-            print('Bye!')
-            exit()
-
-        if type(guess) == int:
             # player tried again
             tries += 1
             # check against answer
             correct = compare.stuff(int(guess),answer)
-    
+
             # player won, start a new game
             if correct:
                 break
-    
+
             # player ran out of tries, start a new game
             if tries == 10:
                 print('Sorry, your 10 tries are up. The number was %s. Lets play again.' % answer)
                 break
+
+        #checks value if it contains ANY letters in it
+        else:
+            if guess.isalpha():
+                guess = str(guess)
+                # exit if player hits q
+                if guess == 'q':
+                    print('Bye!')
+                    exit()
+                else:
+                    print('This command is invalid')
