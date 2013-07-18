61c7e5168a6872d8a23b7733968633c1d1fe592a
Hurshal Patel <hurshal.patel@adroll.com>
2013-07-18 12:48:22 -0700

    update from source

--- run_this_file.py
+++ run_this_file.py
@@ -15,23 +15,38 @@ while True:
     tries = 0
     guess = None
     while guess != answer:
-        guess = input('What number am I thinking of: ')
+        guess = raw_input('What number am I thinking of: ')
+        #checks value if can be converted into an integer 
+        #and converts it if possible
+        if guess.isdigit():
+            guess = int(guess)
+        #checks value if it contains ANY letters in it
+        if type(guess) == str:
+            if guess.isalpha():
+                guess = str(guess)
+                if guess == 'q':
+                    lol = 'lmfao'
+                else:
+                    print('This command is invalid')
+            
+        
 
         # exit if player hits q
         if guess == 'q':
             print('Bye!')
             exit()
 
-        # player tried again
-        tries += 1
-        # check against answer
-        correct = compare.stuff(int(guess),answer)
-
-        # player won, start a new game
-        if correct:
-            break
-
-        # player ran out of tries, start a new game
-        if tries == 10:
-            print('Sorry, your 10 tries are up. The number was %s. Lets play again.' % answer)
-            break
+        if type(guess) == int:
+            # player tried again
+            tries += 1
+            # check against answer
+            correct = compare.stuff(int(guess),answer)
+    
+            # player won, start a new game
+            if correct:
+                break
+    
+            # player ran out of tries, start a new game
+            if tries == 10:
+                print('Sorry, your 10 tries are up. The number was %s. Lets play again.' % answer)
+                break
